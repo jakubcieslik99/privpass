3d8d3cf34daf731624d1cf82dd10eb295f9528c1
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2024-01-15 23:44:50 +0100

    Updated client dependencies & linting rules

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -7,5 +7,8 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'arrow-parens': ['error', 'as-needed'],
   },
 }
