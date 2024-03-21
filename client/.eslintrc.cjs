7c86066e1cefc8b2e9f1c98feb56daecf6a9bdf3
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2024-03-22 00:14:49 +0100

    Updated client project dependencies, tsconfig, prettierrc & eslintrc files

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,14 +1,22 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    es2020: true,
+    browser: true,
+  },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'prettier'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: '2020',
+    sourceType: 'module',
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     semi: ['error', 'never'],
     quotes: ['error', 'single'],
     'arrow-parens': ['error', 'as-needed'],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
   },
+  root: true,
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
 }
