2df585c54056432e9b60a8b4c43fd530a13ee847
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2026-02-16 01:44:28 +0100

    Updated client dependencies & linting rules

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'eslint/config'
-import { includeIgnoreFile } from '@eslint/compat'
 import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'eslint/config'
 import globals from 'globals'
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
 import eslintPluginReactRefresh from 'eslint-plugin-react-refresh'
+import { includeIgnoreFile } from '@eslint/compat'
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 import stylistic from '@stylistic/eslint-plugin'
 
@@ -14,16 +14,17 @@ const gitignorePath = fileURLToPath(new URL('../.gitignore', import.meta.url))
 
 export default defineConfig([
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
+  js.configs.recommended,
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  eslintPluginPrettier,
   eslintPluginReactRefresh.configs.recommended,
-  eslintPluginReactHooks.configs['recommended-latest'],
+  eslintPluginPrettier,
   includeIgnoreFile(gitignorePath),
-  { plugins: { '@stylistic': stylistic } },
   {
+    plugins: { 'react-hooks': eslintPluginReactHooks, '@stylistic': stylistic },
+    settings: { react: { version: 'detect' } },
     rules: {
+      ...eslintPluginReactHooks.configs['recommended-latest'].rules,
       'no-unused-vars': 'off',
       'no-console': ['error', { allow: ['log', 'info', 'warn', 'error'] }],
       '@stylistic/semi': ['error', 'never'],
